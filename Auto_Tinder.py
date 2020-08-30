ce4d93e3766dea38ea886b64d04c01bed32d0360
Md. Samshad Rahman <samshad.ru@gmail.com>
2020-08-30 16:34:17 +0600

    bio and tags

--- Auto_Tinder.py
+++ Auto_Tinder.py
@@ -86,8 +86,13 @@ class AutoTinder:
             sleep(2)
             name = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/div/h1').text
             age = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div[1]/span').text
-            tags = self.driver.find_elements_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div[2]/div[3]')
-            bio = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[2]/div').text
+            tags = []
+            bio = ''
+            try:
+                tags = self.driver.find_elements_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[1]/div/div[2]/div[3]')
+                bio = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[2]/div[2]/div').text
+            except:
+                pass
             if tags:
                 for tag in tags:
                     print(tag)
